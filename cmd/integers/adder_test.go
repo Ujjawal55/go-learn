e82b580a04ea02c302802ab1d4facf25dadbb2e3
ujjawal <ujjawal17032002@gmail.com>
2025-01-08 05:29:33 +0530

    add example in test learn pkgsite

--- cmd/integers/adder_test.go
+++ cmd/integers/adder_test.go
@@ -1,6 +1,13 @@
 package integers
 
 import "testing"
+import "fmt"
+
+func ExampleAdd() {
+    sum := Add(1,5)
+    fmt.Println(sum)
+    //output 6
+}
 
 func TestAdder(t *testing.T) {
     got := Add(2,2)
