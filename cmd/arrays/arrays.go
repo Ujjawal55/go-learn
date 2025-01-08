1d722c292a9b8cb44611b12245fdfe006d91a2fa
ujjawal <ujjawal17032002@gmail.com>
2025-01-08 16:07:17 +0530

    refactoring of code

--- cmd/arrays/arrays.go
+++ cmd/arrays/arrays.go
@@ -1,10 +1,10 @@
 package arrays
 
-func Sum(numbers [5]int) int {
-    sum := 0
-    for i := 0; i < 5; i++ {
-        sum += numbers[i]
-    }
+func Sum(numbers []int) int {
+    var sum int = 0
 
+    for _, num := range numbers {
+        sum += num
+    }
     return sum
 }
--- cmd/arrays/arrays_test.go
+++ cmd/arrays/arrays_test.go
@@ -4,12 +4,29 @@ package arrays
 import "testing"
 
 func TestSum(t *testing.T) {
-    numbers := [5]int{1,2,3,4,5}
+    // Run("fixed capacity array", func (t *testing.T) {
+    //     numbers := [5]int{1,2,3,4,5}
+    //
+    //     got := Sum(numbers)
+    //     want := 15
+    //
+    //     assertCorrectionAnswer(t, got, want)
+    // })
 
-    got := Sum(numbers)
-    want := 15
+    t.Run("variable size array(slice)", func (t *testing.T) {
+        var numbers []int = []int{1,2,3,4,5}
 
+        var got int = Sum(numbers)
+        var want int = 15
+
+        assertCorrectionAnswer(t, got, want)
+    })
+
+}
+
+func assertCorrectionAnswer(t testing.TB, got int, want int) {
+    t.Helper()
     if got != want {
-        t.Errorf("got: %q, want: %v", got, want)
+        t.Errorf("got: %v, want: %v", got, want)
     }
 }
