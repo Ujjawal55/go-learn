900ed57cb59bcb30478543ffd454ba24ccef4867
ujjawal <ujjawal17032002@gmail.com>
2025-01-09 11:26:12 +0530

    next add area method

--- cmd/struct_learning/shape.go
+++ cmd/struct_learning/shape.go
@@ -1,14 +1,26 @@
 package struct_learning
 
+
 type Rectangle struct {
     width float64
     height float64
 }
 
+type Circle struct {
+    radius float64
+}
+
+
 func Perimeter(rectangle Rectangle) float64 {
     return 2.0 * rectangle.width * rectangle.height
 }
 
-func Area(rectangle Rectangle) float64 {
-    return rectangle.width * rectangle.height
-}
+// cannot define multiple Area function
+
+// func Area(rectangle Rectangle) float64 {
+//     return rectangle.width * rectangle.height
+// }
+//
+// func Area(circle Circle) float64 {
+//     return pi * circle.radius * circle.radius
+// }
--- cmd/struct_learning/shape_test.go
+++ cmd/struct_learning/shape_test.go
@@ -2,6 +2,11 @@ package struct_learning
 
 import "testing"
 
+
+const pi = 3.141
+
+
+
 func TestPermeter(t *testing.T) {
     rectangle := Rectangle{10.0, 10.0}
     got := Perimeter(rectangle)
@@ -13,12 +18,42 @@ func TestPermeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-    rectangle := Rectangle{10.0, 10.0}
-    var got float64 = Area(rectangle)
-    var want float64 = 10.0 * 10.0
+    t.Run("Rectangle", func(t *testing.T) {
+        rectangle := Rectangle{10.0, 10.0}
+        var got float64 = Area(rectangle)
+        var want float64 = 10.0 * 10.0
 
+        if (got != want) {
+            t.Errorf("got: %f, want: %f", got, want)
+        }
+    })
+
+    t.Run("Circle", func(t *testing.T) {
+        circle := Circle{10.0}
+        got := Area(circle)
+        want := pi * 10.0 * 10.0
+
+        assertArea(t, got, want)
+    })
+
+}
+
+
+func assertArea(t testing.TB, got float64, want float64) {
+    t.Helper()
     if (got != want) {
         t.Errorf("got: %f, want: %f", got, want)
     }
 
 }
+
+
+
+
+
+
+
+
+
+
+
