00baee5751946abd2231e95decb81eb870720b51
ujjawal <ujjawal17032002@gmail.com>
2025-01-09 16:50:39 +0530

    final refactor

--- cmd/struct_learning/shape_test.go
+++ cmd/struct_learning/shape_test.go
@@ -4,41 +4,80 @@ import "testing"
 
 func TestPermeter(t *testing.T) {
 
-    checkPerimeter := func(t testing.TB, shape Shape, want float64) {
-        t.Helper()
-        var got float64 = shape.Perimeter()
-        assertProperty(t, got, want)
+    // idea here is that we have to define list of test cases therefore follow table driven tests
 
+    const pi = 3.141
+
+    perimetertests := []struct {
+        shape Shape
+        want float64
+    }{
+        {Rectangle{10.0, 10.0}, 2.0 * 10.0 * 10.0 },
+        {Circle{10.0}, 2.0 * pi * 10.0},
     }
 
-    rectangle := Rectangle{10.0, 10.0}
-    want := rectangle.Perimeter()
-    checkPerimeter(t, rectangle, want)
+    for _, tt := range perimetertests {
+        got := tt.shape.Perimeter()
+        want := tt.want
+
+        assertProperty(t, got, want)
+    }
+
+    // checkPerimeter := func(t testing.TB, shape Shape, want float64) {
+    //     t.Helper()
+    //     var got float64 = shape.Perimeter()
+    //     assertProperty(t, got, want)
+    //
+    // }
+    //
+    // rectangle := Rectangle{10.0, 10.0}
+    // want := rectangle.Perimeter()
+    // checkPerimeter(t, rectangle, want)
 }
 
 func TestArea(t *testing.T) {
 
-    checkArea := func(t testing.TB, shape Shape, want float64) {
-        t.Helper()
-        got := shape.Area()
 
-        assertProperty(t, got, want)
-
+    const pi = 3.141
+    areaTest := []struct {
+        shape Shape
+        want float64
+    } {
+        {Rectangle{10.0, 10.0}, 10.0 * 10.0},
+        {Circle{10.0}, pi * 10.0 * 10.0},
     }
 
-    t.Run("Rectangle", func(t *testing.T) {
-        rectangle := Rectangle{10.0, 10.0}
-        var want float64 = 10.0 * 10.0
-        checkArea(t, rectangle, want)
+    for _, tt := range areaTest {
+        got := tt.shape.Area()
+        want := tt.want
 
-    })
+        assertProperty(t, got, want)
+    }
 
-    t.Run("Circle", func(t *testing.T) {
-        const pi = 3.141
-        circle := Circle{10.0}
-        want := pi * 10.0 * 10.0
-        checkArea(t, circle, want)
-    })
+
+
+
+    // checkArea := func(t testing.TB, shape Shape, want float64) {
+    //     t.Helper()
+    //     got := shape.Area()
+    //
+    //     assertProperty(t, got, want)
+    //
+    // }
+    //
+    // t.Run("Rectangle", func(t *testing.T) {
+    //     rectangle := Rectangle{10.0, 10.0}
+    //     var want float64 = 10.0 * 10.0
+    //     checkArea(t, rectangle, want)
+    //
+    // })
+    //
+    // t.Run("Circle", func(t *testing.T) {
+    //     const pi = 3.141
+    //     circle := Circle{10.0}
+    //     want := pi * 10.0 * 10.0
+    //     checkArea(t, circle, want)
+    // })
 
 }
 
